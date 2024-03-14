9f2193c50a09d9e5908d0ad4469ea3d3c25ca91a
Md Saifur Rahman <saifurrshishir@gmail.com>
2024-03-14 17:50:56 +0600

    create the card for car

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,10 @@
 export async function fetchCars() {
-    const headers = {
-        headers: {
+    const headers = {     
             'X-RapidAPI-Key': '3342753a4fmsh53ab3f72b0fbf90p1021f4jsn336da53b7817',
-            'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-        }
+            'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'    
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', { 
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', { 
         headers: headers, 
     });
 
@@ -14,3 +12,19 @@ export async function fetchCars() {
 
     return result;
 }
+
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+  
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
