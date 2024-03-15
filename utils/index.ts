3855eb9556c4b0f155e0b6cbb8967d2b1a51219c
Md Saifur Rahman <saifurrshishir@gmail.com>
2024-03-15 14:51:36 +0600

    add the custom filter feature

--- utils/index.ts
+++ utils/index.ts
@@ -1,15 +1,15 @@
 import { CarProps, FilterProps } from "@/types";
 
 export async function fetchCars(filters: FilterProps) {
-    const {manufacturer, limit, year, fuel, model} = filters;
+    const { manufacturer, limit, year, fuel, model } = filters;
 
-    const headers = {     
-            'X-RapidAPI-Key': '3342753a4fmsh53ab3f72b0fbf90p1021f4jsn336da53b7817',
-            'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'    
+    const headers = {
+        'X-RapidAPI-Key': '3342753a4fmsh53ab3f72b0fbf90p1021f4jsn336da53b7817',
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&mode=${model}&limit=${limit}&fuel_type=${fuel}`, { 
-        headers: headers, 
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&mode=${model}&limit=${limit}&fuel_type=${fuel}`, {
+        headers: headers,
     });
 
     const result = await response.json();
@@ -22,24 +22,24 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const basePricePerDay = 50; // Base rental price per day in dollars
     const mileageFactor = 0.1; // Additional rate per mile driven
     const ageFactor = 0.05; // Additional rate per year of vehicle age
-  
+
     // Calculate additional rate based on mileage and age
     const mileageRate = city_mpg * mileageFactor;
     const ageRate = (new Date().getFullYear() - year) * ageFactor;
-  
+
     // Calculate total rental rate per day
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
-  
+
     return rentalRatePerDay.toFixed(0);
-  };
+};
 
 
 
-export const generateCarImageUrl = (car: CarProps, angle?: string) =>{
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
     const url = new URL('https://cdn.imagin.studio/getimage');
 
-    const {make, year, model} = car;
-    
+    const { make, year, model } = car;
+
     url.searchParams.append('customer', 'hrjavascript-mastery')
 
     url.searchParams.append('make', make);
@@ -50,4 +50,15 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) =>{
     url.searchParams.append('make', make);
 
     return `${url}`;
+}
+
+
+export const updateSearchParams = (type: string, value: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    searchParams.set(type, value);
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+    return newPathname;
 }
